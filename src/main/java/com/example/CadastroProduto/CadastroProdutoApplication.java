4be395565094474d44a6ae613127d7ca9eb57f90
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-10 15:48:38 -0300

    projeto criado

---
+++ src/main/java/com/example/CadastroProduto/CadastroProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CadastroProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroProdutoApplication.class, args);
+	}
+
+}
