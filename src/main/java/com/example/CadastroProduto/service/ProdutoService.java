39cc8c2d648b026e4e67e32fcc234683163b10de
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-10 22:40:18 -0300

    find all, findById

---
+++ src/main/java/com/example/CadastroProduto/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package com.example.CadastroProduto.service;
+
+import com.example.CadastroProduto.entities.Produto;
+import com.example.CadastroProduto.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository repository;
+
+    public List<Produto> findAll() {
+        return repository.findAll();
+    }
+
+    public Produto findById(Long id) {
+        Optional<Produto> obj = repository.findById(id);
+        return obj.get();
+    }
+}
